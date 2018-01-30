92f3ce4711c429baef34037a0b155929afe1c1a4
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-30 19:54:09 +0000

    Clean up code and add constructor

--- src/forms/QuantityField.php
+++ src/forms/QuantityField.php
@@ -12,37 +12,56 @@ use SilverStripe\Forms\NumericField;
 class QuantityField extends NumericField
 {
 
+    /**
+     * Construct this field (we override the default to set a default value)
+     *
+     * @param [type] $name
+     * @param [type] $title
+     * @param string $value
+     * @param [type] $maxLength
+     * @param [type] $form
+     */
+    public function __construct($name, $title = null, $value = 0, $maxLength = null, $form = null)
+    {
+        parent::__construct($name, $title, $value);
+    }
+    
     public function Type()
     {
         return 'quantity numeric text';
     }
 
-    /** PHP Validation **/
+    /**
+     * PHP Validation
+     * 
+     * @return boolean
+     * @throws Exception
+    **/
     public function validate($validator)
     {
-        // First check if value is numeric
-        if ($this->value && $this->isNumeric()) {
-            // Convert to a number to check
-            $value = $this->value + 0;
-            
-            if(is_int($value)) {
-                return true;
-            }
+        $value = $this->Value() + 0;
+        
+        if(is_int($value)) {
+            return true;
         }
         
         $validator->validationError(
             $this->name,
             _t(
                 'Checkout.VALIDATION', '{value} is not a valid number, only whole numbers can be accepted for this field',
-                array('value' => $this->value)
+                array('value' => $value)
             ),
             "validation"
         );
+
         return false;
     }
     
     public function dataValue()
     {
-        return (is_numeric($this->value)) ? $this->value : 0;
+        $value = $this->Value();
+        $value =  (is_numeric($value)) ? $value : 0;
+
+        return $this->cast($value);
     }
 }
